a519196bbe2db45845c8458ca4b545d376692df0
juan <juan@Juans-MacBook-Pro.local>
2019-06-21 14:08:46 -0300

    fix field in import

--- api/import.php
+++ api/import.php
@@ -5,7 +5,7 @@ $noLogin = true;
 $noMenu = true;
 $noHeader = true;
 require "../files/php/config/require.php";
-$email = _post("user");
+$email = _post("email");
 $pass = _post("pass");
 $user = Entity::search("User", "email = '$email' AND pass = '$pass' AND active");
 if(!$user){
